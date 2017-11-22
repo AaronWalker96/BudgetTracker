3a32da86412703d7be11667e7dddde619aab76f0
AaronWalker96 <aaron45587@gmail.com>
2017-11-22 23:34:49 +0000

    Added a home screen with a button to add expenses

---
+++ app/src/main/java/comaaronwalker96budgettracker/httpsgithub/budgettracker/AddExpense.java
@@ -0,0 +1,13 @@
+package comaaronwalker96budgettracker.httpsgithub.budgettracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddExpense extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_expense);
+    }
+}
--- app/src/main/java/comaaronwalker96budgettracker/httpsgithub/budgettracker/MainActivity.java
+++ app/src/main/java/comaaronwalker96budgettracker/httpsgithub/budgettracker/MainActivity.java
@@ -1,7 +1,12 @@
 package comaaronwalker96budgettracker.httpsgithub.budgettracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,9 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        int totalBudget;
+    public void addExpenseBtn(View view) {
 
-
+        Intent intent = new Intent(this, AddExpense.class);
+        startActivity(intent);
     }
 }
