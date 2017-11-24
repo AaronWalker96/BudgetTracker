ddcdbf333929dc48c3f3f921c4773563b868dbaf
AaronWalker96 <aaron45587@gmail.com>
2017-11-24 01:32:36 +0000

    Progressed with home screen, adding logic for entering expenses

--- app/src/main/java/comaaronwalker96budgettracker/httpsgithub/budgettracker/AddExpense.java
+++ app/src/main/java/comaaronwalker96budgettracker/httpsgithub/budgettracker/AddExpense.java
@@ -1,7 +1,11 @@
 package comaaronwalker96budgettracker.httpsgithub.budgettracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputFilter;
+import android.view.View;
+import android.widget.EditText;
 
 public class AddExpense extends AppCompatActivity {
 
@@ -10,4 +14,26 @@ public class AddExpense extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_expense);
     }
+
+    public void submitExpenses(View view) {
+        //Screen Elements
+        EditText rentEdit = (EditText)findViewById(R.id.txtRent);
+        EditText utilitiesEdit = (EditText)findViewById(R.id.txtUtilities);
+        EditText foodEdit = (EditText)findViewById(R.id.txtFood);
+        EditText leisureEdit = (EditText)findViewById(R.id.txtLeisure);
+
+        //Get text from text boxes
+        String rentExpenseIn = rentEdit.getText().toString();
+        String utilitiesExpenseIn = utilitiesEdit.getText().toString();
+        String foodExpenseIn = foodEdit.getText().toString();
+        String leisureExpenseIn = leisureEdit.getText().toString();
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("Rent_Expense", rentExpenseIn);
+        intent.putExtra("Utilities_Expense", utilitiesExpenseIn);
+        intent.putExtra("Food_Expense", foodExpenseIn);
+        intent.putExtra("Leisure_Expense", leisureExpenseIn);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/comaaronwalker96budgettracker/httpsgithub/budgettracker/MainActivity.java
+++ app/src/main/java/comaaronwalker96budgettracker/httpsgithub/budgettracker/MainActivity.java
@@ -4,21 +4,61 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
+import android.widget.TextView;
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Vars
+        double rentExpense = 0;
+        double utilitiesExpense = 0;
+        double foodExpense = 0;
+        double leisureExpense = 0;
+
+        //Get data passed from Add Expense activity
+        Intent intent = getIntent();
+
+        //The following requires fixing--------------------------------------------------------------------------------------------------------------
+
+//        //Only do the following if data is passed, this prevents the app from crashing on startup or if no data was provided
+//        if (intent.getStringExtra("Rent_Expense") != null) {
+//            //collect all of the variables passed through from the add expense activity, try catch is used if the vars do not contain integers
+//            try {
+//                rentExpense = Double.parseDouble(getIntent().getStringExtra("Rent_Expense"));
+//                utilitiesExpense = Double.parseDouble(getIntent().getStringExtra("Utilities_Expense"));
+//                foodExpense = Double.parseDouble(getIntent().getStringExtra("Food_Expense"));
+//                leisureExpense = Double.parseDouble(getIntent().getStringExtra("Leisure_Expense"));
+//
+//                double totalExpense;
+//
+//                totalExpense = rentExpense + utilitiesExpense + foodExpense + leisureExpense;
+//                DecimalFormat df = new DecimalFormat("####0.00");
+//                String totalExpenseFormatted = "Total Expenses: £" + df.format(totalExpense);
+//
+//                TextView expensesText = (TextView)findViewById(R.id.txtExpenses);
+//                expensesText.setText(totalExpenseFormatted);
+//
+//            } catch(NumberFormatException nfe) {
+//
+//            }
+//
+//        }
     }
 
     public void addExpenseBtn(View view) {
-
         Intent intent = new Intent(this, AddExpense.class);
         startActivity(intent);
     }
+
+    public void addIncomeBtn(View view) {
+        Intent intent = new Intent(this, AddIncome.class);
+        startActivity(intent);
+    }
+
 }
